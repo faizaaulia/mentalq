b7d47e81e5993eef77e1a9f830da210a12fee0a7
Masruri <alviyantoruri@gmail.com>
2020-04-23 01:33:31 +0700

    Ganti nama fungsi

--- application/controllers/Patient.php
+++ application/controllers/Patient.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Patient extends CI_Controller {
-   public function Consult()
+   public function consultPatient()
    {
       $this->load->view('ConsultPasien');
    }
