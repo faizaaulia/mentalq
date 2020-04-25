3e52765f9f3a30e7cea784e096682587fe688e85
faizaaulia <faizaaulia20@gmail.com>
2020-04-25 15:32:27 +0700

    remove duplicate function
    beautify file

--- application/controllers/Consultant.php
+++ application/controllers/Consultant.php
@@ -2,80 +2,57 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Consultant extends CI_Controller {
-		    public function __construct()
-		    {
-		       parent::__construct();
-		       $this->load->model('Consultant_model');
-		    }
+	public function __construct()
+	{
+	    parent::__construct();
+		$this->load->model('Consultant_model');
+    }
+    
+	public function index() {
+        $this->load->view('welcome_message');
+    }
 
-		    public function Consultant() {
-		        $data['consultants'] = $this->Consultant_model->getDataConsultants();
-		        $this->load->view('Consultant', $data);
-		    }
+	public function Consultant() {
+	    $data['consultants'] = $this->Consultant_model->getDataConsultants();
+		$this->load->view('Consultant', $data);
+	}
 
-		    public function showConsultant($noSTR)
-		    {
-		        $res['profile'] = $this->Consultant_model->getConsultantProfile($noSTR);
-		        $res['baseURL'] = base_url('assets/img/Consultants/');
-		        echo json_encode($res);
+	public function showConsultant($noSTR) {
+		$res['profile'] = $this->Consultant_model->getConsultantProfile($noSTR);
+        $res['baseURL'] = base_url('assets/img/Consultants/');
+		echo json_encode($res);
+    }
 
-		    }
-
-				public function profile(){
-		        $this->load->view('profile_psikolog');
-		    }
-
-				public function index()
-				{
-
-					$this->load->view('welcome_message');
-				}
-
-
-
-
-			    public function showProfile(){
-					$data['consultants']= $this->Consultant_model->getDataConsultant();
-			        $this->load->view('profile_psikolog',$data);
-                }
+	public function profile(){
+		$this->load->view('profile_psikolog');
+	}
                 
-                // Mendapat Keliuhan dari Consults
-                public function replyConsult()
-                {
-                    $data['consult'] = $this->Consultant_model->getConsults();
-                    $this->load->view('consult_consultant',$data);
-                }
+	public function showProfile() {
+		$data['consultants']= $this->Consultant_model->getDataConsultant();
+		$this->load->view('profile_psikolog',$data);
+    }
+                
+    // Mendapat Keliuhan dari Consults
+    public function replyConsult() {
+        $data['consult'] = $this->Consultant_model->getConsults();
+        $this->load->view('consult_consultant',$data);
+    }
         
         
-                // Mengeset Profil yang sudah diisi di form Edit Profil
-                public function setReplyConsults()
-                {
-                    $this->Consultant_model->setReplyConsults();
-                }
+    // Mengeset Profil yang sudah diisi di form Edit Profil
+    public function setReplyConsults() {
+        $this->Consultant_model->setReplyConsults();
+    }
         
-                // Mendapat Profil dari Consultant
-                public function updateProfile()
-                {
-                    $data['profile'] = $this->Consultant_model->getProfileConsultant();
-                    $this->load->view('edit_profile_consultant',$data);
-                }
+    // Mendapat Profil dari Consultant
+    public function updateProfile() {
+        $data['profile'] = $this->Consultant_model->getProfileConsultant();
+        $this->load->view('edit_profile_consultant',$data);
+    }
         
-                // Mengeset Profil yang sudah diisi di form Edit Profil
-                public function setUpdateProfile()
-                {
-                    $this->Consultant_model->setUpdateProfile();
-                }
-        
-                public function Consultant() {
-                    $data['consultants'] = $this->Consultant_model->getDataConsultants();
-                    $this->load->view('Consultant', $data);
-                }
-            
-                public function showConsultant($noSTR)
-                {
-                    $res['profile'] = $this->Consultant_model->getConsultantProfile($noSTR);
-                    $res['baseURL'] = base_url('assets/img/Consultants/'); 
-                    echo json_encode($res);
-                    
-                }
+    // Mengeset Profil yang sudah diisi di form Edit Profil
+    public function setUpdateProfile() {
+        $this->Consultant_model->setUpdateProfile();
+    }
+    
 }
--- application/models/Patient_model.php
+++ application/models/Patient_model.php
@@ -1,8 +1,4 @@
 <?php
-
-
-
-
 class Patient_model extends CI_Model{
     public function insertKeluhan() {
         $data = array (
@@ -11,45 +7,41 @@ class Patient_model extends CI_Model{
             'keluhan' => $this->input->post('keluhan'),
             'solusi' => '-',
             'status' => '0'
-            );
-         $this->db->insert('consults',$data);
+        );
+        $this->db->insert('consults',$data);
     }
 
     Public function getDataPatients() {
         $username = 1;
         return $this->db->where('idPasien',$username)->get('patients')->row();
-
     }
 
-    Public function getConsult(){
+    Public function getConsult() {
         $this->db->select('*')
 		//  ->select('consultants.namaConsultant', 'patients.namaPasien')
-         ->from('consults')
-         ->where('consults.idPasien', 1)
-         ->join('consultants', 'consultants.noSTR = consults.noSTR')
-         ->join('patients', 'patients.idPasien = consults.idPasien');
+                 ->from('consults')
+                 ->where('consults.idPasien', 1)
+                 ->join('consultants', 'consultants.noSTR = consults.noSTR')
+                 ->join('patients', 'patients.idPasien = consults.idPasien');
         return $this->db->get();
     }
 
-		public function getProfilePatient(){
-				$idPasien= '1';
-				return $this->db->where('idPasien',$idPasien)->get('patients')->row();
-		}
+	public function getProfilePatient() {
+	    $idPasien= '1';
+		return $this->db->where('idPasien',$idPasien)->get('patients')->row();
+	}
 
 
-		//Mendapatkan data terbaru dari Pasien
-		public function ubahDataPasien()
-		{
-				$data =array(
-
-						'email' => $this->input->post('email'),
-						'alamat' => $this->input->post('alamat'),
-						'noHP' => $this->input->post('noHP'),
-						'umur' => $this->input->post('umur'),
+	//Mendapatkan data terbaru dari Pasien
+	public function ubahDataPasien() {
+	    $data = array(
+                    'email' => $this->input->post('email'),
+					'alamat' => $this->input->post('alamat'),
+					'noHP' => $this->input->post('noHP'),
+					'umur' => $this->input->post('umur'),
 				);
-				//use query builder class to update data patient based on idPasien
-				 $this->db->where('idPasien','1')->update('patients',$data);
-		}
-
+		//use query builder class to update data patient based on idPasien
+		$this->db->where('idPasien','1')->update('patients',$data);
+	}
 }
 ?>
